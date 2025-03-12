9be1c1d8e078e7c5b73727efb0eb7a88636336a8
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-03-12 15:49:28 +0000

    J'ai crée une migration pour chaque class dans notre diagramme des classes et chaque migration avec un model

---
+++ database/migrations/2025_03_12_153312_create_wallets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->float('total');
+            $table->foreignId('utilisateur')->constrained();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
---
+++ database/migrations/2025_03_12_154501_create_historiques_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historiques', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->string('status');
+            $table->date('date');
+            $table->float('total');
+            $table->foreignId('transaction')->constrained('transactions');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historiques');
+    }
+};
